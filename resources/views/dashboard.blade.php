0f20203b2a31d295b42086cc52709f9c085041c5
Zebedeus12 <nabillazebedeus@gmail.com>
2024-10-22 11:06:56 +0700

    menambahkan autenthikasi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="container">
+    <h1>Selamat Datang di Dashboard!</h1>
+    <p>Ini adalah halaman dashboard yang hanya bisa diakses oleh pengguna yang telah login.</p>
+</div>
+@endsection
