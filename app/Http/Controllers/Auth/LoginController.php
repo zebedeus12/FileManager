fe9b3ab9179490179799264a6a2bf1f9355df8d9
Zebedeus12 <nabillazebedeus@gmail.com>
2024-10-22 12:06:14 +0700

    membuat dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,4 +42,21 @@ class LoginController extends Controller
         Auth::logout();
         return redirect()->route('login'); // Ganti 'login' sesuai rute yang diinginkan
     }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah.',
+        ]);
+    }
 }
